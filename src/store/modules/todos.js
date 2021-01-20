9fee194110c5b0c0f7278952ab5d554936aafe14
LexiosAlex <lexiosma@gmail.com>
2021-01-20 19:19:31 +0300

    vuex: moved actions to vuex

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -10,6 +10,12 @@ export default {
             ctx.commit("updateTodos", json);
           }, 1000);
         });
+    },
+    removeTodo(ctx, { id }) {
+      ctx.commit("removeTodo", id);
+    },
+    addTodo(ctx, { todo }) {
+      ctx.commit("addTodo", todo);
     }
   },
   mutations: {
@@ -21,18 +27,23 @@ export default {
     },
     fetchTodosSucceed(state) {
       state.loading = false;
+    },
+    removeTodo(state, id) {
+      state.todos = state.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(state, todo) {
+      state.todos.push(todo);
     }
   },
   state: {
     todos: [],
-    loading: true
+    loading: false
   },
   getters: {
     getAllTodos(state) {
       return state.todos;
     },
     getIsLoading(state) {
-      console.log(state);
       return state.loading;
     }
   }
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -12,9 +12,9 @@
     <hr />
     <Loader v-if="getIsLoading" />
     <TodoList
-      v-bind:todos="getAllTodos"
+      v-bind:todos="filteredTodos"
       @remove-todo="removeTodo"
-      v-else-if="getAllTodos.length"
+      v-else-if="filteredTodos.length"
     />
     <p v-else>No todos!</p>
   </section>
@@ -28,21 +28,21 @@ import { mapGetters } from "vuex";
 export default {
   name: "Todos",
   computed: {
-    // filteredTodos() {
-    //   if (this.filter === "all") {
-    //     return this.getAllTodos;
-    //   }
-    //
-    //   if (this.filter === "completed") {
-    //     return this.getAllTodos.filter(todo => todo.completed);
-    //   }
-    //
-    //   if (this.filter === "not-completed") {
-    //     return this.getAllTodos.filter(todo => !todo.completed);
-    //   }
-    //
-    //   return this.getAllTodos;
-    // },
+    filteredTodos() {
+      if (this.filter === "all") {
+        return this.getAllTodos;
+      }
+
+      if (this.filter === "completed") {
+        return this.getAllTodos.filter(todo => todo.completed);
+      }
+
+      if (this.filter === "not-completed") {
+        return this.getAllTodos.filter(todo => !todo.completed);
+      }
+
+      return this.getAllTodos;
+    },
     ...mapGetters(["getAllTodos", "getIsLoading"])
   },
   data() {
@@ -57,21 +57,13 @@ export default {
   },
   mounted() {
     this.$store.dispatch("fetchTodos");
-    // fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     setTimeout(() => {
-    //       this.todosArray = json;
-    //       this.loading = false;
-    //     }, 1000);
-    //   });
   },
   methods: {
     removeTodo(id) {
-      this.todosArray = this.todosArray.filter(todo => todo.id !== id);
+      this.$store.dispatch("removeTodo", { id });
     },
     addTodo(todo) {
-      this.todosArray.push(todo);
+      this.$store.dispatch("addTodo", { todo });
     }
   }
 };
